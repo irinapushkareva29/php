470f1beec1bbc13fdba97b12117e5a4aa0762884
Irina <irinapushkareva29@gmail.com>
2023-08-09 10:28:46 +0300

    add describe of buttons

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -12,9 +12,37 @@ orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
+    minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
+    maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+    alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+    answerNumber  = Math.floor((minValue + maxValue) / 2);
+
     orderNumber = 0;
+    orderNumber++;
+    orderNumberField.innerText = orderNumber;
+
+    answerField.innerText = `Вы загадали число ${answerNumber }?`;
+    gameRun = true;
+})
+
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random());
+            const answerPhrase = (phraseRandom === 1) ?
+                `Вы загадали неправильное число!\n\u{1F914}` :
+                `Я сдаюсь..\n\u{1F92F}`;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber - 1;
+            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -38,8 +66,26 @@ document.getElementById('btnOver').addEventListener('click', function () {
 })
 
 document.getElementById('btnEqual').addEventListener('click', function () {
-    if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+    if (gameRun) {
+        const phraseRandom = Math.round(Math.random() * 3);
+        switch (phraseRandom) {
+            case 0:
+                answerPhrase = `Я всегда угадываю\n\u{1F60E}`
+                break;
+
+            case 1:
+                answerPhrase = `Ты прав! \n\u{1F60E}`
+                break;
+
+            case 2:
+                answerPhrase = `Молодец!\n\u{1F973}`
+                break;
+
+            case 3:
+                answerPhrase = `Прими поздравления!\n\u{1F929}`
+                break;
+        }
+        answerField.innerText = answerPhrase;
         gameRun = false;
     }
 })
